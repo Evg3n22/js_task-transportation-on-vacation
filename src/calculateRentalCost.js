6e621d8adcbb99f8346566bb2c77032bf34e8a47
Evg3n22 <evgen.gulevaty@gmail.com>
2025-10-29 18:42:58 +0200

    fix issues

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,15 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  let res = days * 40;
+  const COST_PER_DAY = 40;
+  let totalCost = days * COST_PER_DAY;
 
   if (days >= 7) {
-    res -= 50;
-  } else if (days >= 3) {
-    res -= 20;
+    totalCost -= 50;
+
+    return totalCost;
   }
 
-  return res;
+  if (days >= 3) {
+    totalCost -= 20;
+
+    return totalCost;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
