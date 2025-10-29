dafa6a2bab553b268d5ed7a8ca96fed27b4bf7a3
Evg3n22 <evgen.gulevaty@gmail.com>
2025-10-29 19:52:43 +0200

    replace magic numbers

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,16 +6,20 @@
 function calculateRentalCost(days) {
   // write code here
   const COST_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
   let totalCost = days * COST_PER_DAY;
 
-  if (days >= 7) {
-    totalCost -= 50;
+  if (days >= LONG_TERM) {
+    totalCost -= LONG_TERM_DISCOUNT;
 
     return totalCost;
   }
 
-  if (days >= 3) {
-    totalCost -= 20;
+  if (days >= SHORT_TERM) {
+    totalCost -= SHORT_TERM_DISCOUNT;
 
     return totalCost;
   }
